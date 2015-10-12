5d28acbd918bacd4682045a82c696ea5eee07bf3
oleg095 <oleg095@ukr.net>
2015-10-12 21:57:53 +0300

    Lemke-Shpilberh

---
+++ Lemke-Shpilberh/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lemke_Shpilberh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
